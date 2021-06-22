6d77b40172638b719017378ae63c01d817e40848
Mateo Emmanuel Rodriguez <agusssd0@gmail.com>
2021-06-22 00:49:42 -0300

    Create Tp5ej5.py

---
+++ Tp5ej5.py
@@ -0,0 +1,15 @@
+#################
+#Mateo2021-icomp
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+# Inversion de Mayusculas
+
+def prueba():
+    texto = str(input('Escriba algo: '))
+    texto_swaped = texto.swapcase()
+    print(texto_swaped)
+    pass
+
+if __name__ == "__main__":
+    prueba()
